0f72a391b6f3a1892bb5e09f7f8b8bbb226f3b57
BenEmdon <benjaminemdon@gmail.com>
2016-12-22 15:41:13 -0500

    The refactor begins 🤓

---
+++ Carousel/CenteredCollectionView.swift
@@ -0,0 +1,13 @@
+//
+//  CenteredCollectionView.swift
+//  Carousel
+//
+//  Created by Benjamin Emdon on 2016-12-22.
+//  Copyright © 2016 Benjamin Emdon.
+//
+
+import UIKit
+
+class CenteredCollectionView: UICollectionView {
+	
+}
