5f8310c877cff5c22868eaaa98ddde2d20762293
BenEmdon <benjaminemdon@gmail.com>
2016-12-28 02:25:47 -0500

    Implemented example app

---
+++ Example/Pods/Target Support Files/CenteredCollectionView/CenteredCollectionView-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double CenteredCollectionViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char CenteredCollectionViewVersionString[];
+
