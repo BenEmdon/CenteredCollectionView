b779cc253fdc445a994deb41a1324d4f30facc38
BenEmdon <benjaminemdon@gmail.com>
2017-01-12 00:28:37 -0500

    removed comment from test

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -58,11 +58,4 @@ class Tests: XCTestCase {
 		XCTAssertTrue(collectionView.delegateInterceptor == nil)
 	}
 
-	//	func testPerformanceExample() {
-	//		// This is an example of a performance test case.
-	//		self.measure {
-	//			// Put the code you want to measure the time of here.
-	//		}
-	//	}
-
 }
