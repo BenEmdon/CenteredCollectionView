eb2a1a4bec5377039f03fe3d5c00e90d42d6b7f8
BenEmdon <benjaminemdon@gmail.com>
2017-12-29 17:54:03 -0500

    Updated to swift 4

--- Example/CenteredCollectionView/UIColor+Extensions.swift
+++ Example/CenteredCollectionView/UIColor+Extensions.swift
@@ -10,10 +10,10 @@ import UIKit
 
 extension UIColor {
 	static var peach: UIColor {
-		return  UIColor(colorLiteralRed: 1, green: 0.5764705882, blue: 0.5843137255, alpha: 1)
+		return  UIColor(red: 1, green: 0.5764705882, blue: 0.5843137255, alpha: 1)
 	}
 
 	static var orange: UIColor {
-		return UIColor(colorLiteralRed: 1, green: 0.5764705882, blue: 0.462745098, alpha: 1)
+		return UIColor(red: 1, green: 0.5764705882, blue: 0.462745098, alpha: 1)
 	}
 }
